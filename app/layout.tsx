af5729accc52e85257055b9808c951affef812a8
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-01-10 13:07:32 +0530

    Complete setup of trpc

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
+import { TRPCProvider } from "@/trpc/client";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -19,13 +20,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-        <Toaster position="top-center" />
-      </body>
-    </html>
+    <TRPCProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className} antialiased`}
+        >
+          {children}
+          <Toaster position="top-center" />
+        </body>
+      </html>
+    </TRPCProvider>
   );
 }
--- modules/home/ui/views/home-view.tsx
+++ modules/home/ui/views/home-view.tsx
@@ -1,30 +1,17 @@
 "use client"
-import React from 'react'
-import { authClient } from '@/lib/auth-client'
-import { Button } from '@/components/ui/button'
-import { useRouter } from 'next/navigation'
+
+import { trpc } from "@/trpc/client"
+
+
 
 
 
 export const HomeView = () => {
-    const router = useRouter();
-    const { data: session } = authClient.useSession();
+    const hello = trpc.hello.useQuery({ text: "from tRPC" })
 
-    if (!session) {
-        return (
-            <p>Please sign in to continue.</p>
-        )
-    }
     return (
-        <div>
-            <div className='text-center mt-20'>
-                <h1>Welcome, {session.user.name}</h1>
-                <Button onClick={() => authClient.signOut({
-                    fetchOptions: {
-                        onSuccess: () => router.push('/sign-in'),
-                    }
-                })}>Sign Out</Button>
-            </div>
+        <div className="flex flex-col items-center justify-center p-4">
+            {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading...</p>}
         </div>
     )
 }
