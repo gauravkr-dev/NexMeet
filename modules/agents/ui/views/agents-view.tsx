819182b652e0820cfb67b8fec7e342509d3bcf3c
Gaurav Kumar <gauravkumar803109@gmail.com>
2026-01-11 10:45:02 +0530

    Add agent data table and empty state components

--- modules/agents/ui/views/agents-view.tsx
+++ modules/agents/ui/views/agents-view.tsx
@@ -3,6 +3,9 @@
 import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { trpc } from "@/trpc/client"
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
 
 export const AgentsView = () => {
     const trpcClient = trpc.agents.getMany.useQuery();
@@ -22,8 +25,14 @@ export const AgentsView = () => {
         />;
     }
     return (
-        <div>
-            {JSON.stringify(data, null, 2)}
+        <div className="flex-1 px-4 pb-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data ?? []} columns={columns} />
+            {data?.length === 0 && (
+                <EmptyState
+                    title="No Agents Found"
+                    description="There are no agents to display at the moment."
+                />
+            )}
         </div>
     )
 }
